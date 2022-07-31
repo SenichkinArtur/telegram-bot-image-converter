3f74bed14aa790b420bf0d2cd86c691b5ff2462e
Senichkin Artur <asylumfight28@gmail.com>
2022-08-01 01:21:55 +0300

    Removed unnecessary env variables; added git ignore

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const http = require('http');
 
 require('dotenv').config();
 
-const protocol = process.env.PROTOCOL;
-const hostname = process.env.HOSTNAME;
 const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
@@ -13,5 +11,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, null, () => {
-  console.log(`Server running on ${protocol}://${hostname}:${port}`);
+  console.log(`Server running on port ${port}`);
 });
